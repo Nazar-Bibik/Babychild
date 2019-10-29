308834de1709654f14b7fe575c65ba94bdffb905
Mowsumi <mowrahman007@gamil.com>
2019-10-29 15:28:53 +0100

    Adding screen structure for easy variables

---
+++ BabyChild/Models/Screen.swift
@@ -0,0 +1,23 @@
+//
+//  Screen.swift
+//  BabyChild
+//
+//  Created by Mow on 28/10/2019.
+//  Copyright © 2019 Mow. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Screen {
+    static var width: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    static var height: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    static var statusBarHeight: CGFloat {
+        return UIApplication.shared.statusBarFrame.size.height
+        
+    }
+}
