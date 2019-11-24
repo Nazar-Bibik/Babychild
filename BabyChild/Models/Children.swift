0aa36f61c2c4547f59012d62122a3bf7482bf35d
Mowsumi <mowrahman007@gamil.com>
2019-11-24 01:16:17 +0100

    Creating full operating data storage CoreData

---
+++ BabyChild/Models/Children.swift
@@ -0,0 +1,13 @@
+//
+//  Children.swift
+//  BabyChild
+//
+//  Created by Mow on 24/11/2019.
+//  Copyright © 2019 Mow. All rights reserved.
+//
+
+import Foundation
+
+class Children {
+    var name: String = "Yes"
+}
