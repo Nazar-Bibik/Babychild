fe90cdfd460aee6722bef47e28478a4c6f374b1b
Nazar-Bibik <nazyiato789@gmail.com>
2019-12-16 15:29:22 +0100

    memos notes

---
+++ BabyChild/MemoNotesView.swift
@@ -0,0 +1,33 @@
+//
+//  MemoNotesView.swift
+//  BabyChild
+//
+//  Created by Nazar on 16/12/2019.
+//  Copyright © 2019 Mow. All rights reserved.
+//
+
+import SwiftUI
+
+struct MemoNotesView: View {
+    @State var notes: [Note]
+    @EnvironmentObject var child: Child
+    
+    var body: some View {
+        List{
+            ForEach(notes, id: \.self) {note in
+                VStack(alignment: .leading){
+                        Text(self.child.showName() + "'s " + note.name!)
+                    .font(.headline)
+                        Text(DateHelper.showDate(datetime: note.time!))
+                    .fontWeight(.light)
+                }
+            }
+        }
+    }
+}
+
+//struct MemoNotesView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MemoNotesView()
+//    }
+//}
